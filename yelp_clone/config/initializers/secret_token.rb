75ed85e2ce6c3c01fee78b90bd17a3058320ac5c
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-14 15:45:08 +0000

    server up and running

---
+++ yelp_clone/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpClone::Application.config.secret_key_base = 'c07769e91c107755e2606e8700e100f5b1ab1325d9e6fa894dfdad9011c40384a49ed55c177c24b1bbf4fe083591e9e48daffc16f2a5399fe819b5badc4a9c50'
