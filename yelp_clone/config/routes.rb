08492ca4e4b38b950b9d48c3a0102a1e3192c5ba
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-14 16:49:03 +0000

    add to routes.rb

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,4 +1,5 @@
 YelpClone::Application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,12 +8,14 @@ YelpClone::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  # get 'restaurants' => 'restaurants#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :restaurants
 
   # Example resource route with options:
   #   resources :products do
@@ -39,7 +42,7 @@ YelpClone::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
